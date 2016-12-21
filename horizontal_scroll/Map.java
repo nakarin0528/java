a53c682306cee618c154ab172b31a1a5dbfbe48d
nakarin0528 <n1510121@edu.cc.uec.ac.jp>
2016-12-21 23:35:58 +0900

    first commit

---
+++ horizontal_scroll/Map.java
@@ -0,0 +1,109 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+
+public class Map {
+
+    //ブロックサイズ
+    public static final int TILE_SIZE = 32;
+    //行数列数
+    public static final int ROW = 20;
+    public static final int COL = 30;
+    //幅・高さ
+    public static final int WIDTH = TILE_SIZE*COL;
+    public static final int HEIGHT = TILE_SIZE*ROW;
+    //重力
+    public static final double GRAVITY = 1.5;
+    
+    //マップ
+    private int[][] map = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1},
+    {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0},
+    {1,0,0,0,0,0,1,1,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+    
+    public Map(){}
+    
+    public void draw(Graphics g, int offsetX, int offsetY){
+        //offsetを元に描画範囲を算出
+        int firstTileX = pixelsToTiles(-offsetX);
+        int lastTileX = firstTileX + pixelsToTiles(MainPanel.WIDTH) + 1;
+        int firstTileY = pixelsToTiles(-offsetY);
+        int lastTileY = firstTileY + pixelsToTiles(MainPanel.HEIGHT) + 1;
+        //描画範囲をマップより小さくしとく
+        lastTileX = Math.min(lastTileX, COL);
+        lastTileY = Math.min(lastTileY, ROW);
+        
+        g.setColor(Color.BLACK);
+        for (int i = firstTileY; i<lastTileY; i++){
+            for (int j = firstTileX; j<lastTileX; j++){
+                //mapの値に応じて描画
+                switch (map[i][j]) {
+                    case 1:
+                        g.fillRect(tilesToPixels(j) + offsetX, tilesToPixels(i) + offsetY, TILE_SIZE, TILE_SIZE);
+                        break;
+                }
+            }
+        }
+    }
+    
+    public Point getTileCollision(Player player, double newX,double newY){
+        
+        newX = Math.ceil(newX);
+        newY = Math.ceil(newY);
+        
+        double fromX = Math.min(player.getX(), newX);
+        double fromY = Math.min(player.getY(), newY);
+        double toX = Math.max(player.getX(), newX);
+        double toY = Math.max(player.getY(), newY);
+        
+        int fromTileX = pixelsToTiles(fromX);
+        int fromTileY = pixelsToTiles(fromY);
+        int toTileX = pixelsToTiles(toX + Player.WIDTH - 1);
+        int toTileY = pixelsToTiles(toY + Player.HEIGHT - 1);
+        
+        //衝突判定
+        for (int x = fromTileX; x<=toTileX; x++){
+            for(int y = fromTileY; y<=toTileY;y++){
+                //マップ外は衝突
+                if(y<0 || x>=COL){
+                    return new Point(x,y);
+                }
+                //ブロック衝突
+                if(map[y][x] == 1){
+                    return new Point(x,y);
+                }
+            }
+        }
+        return null;
+    }
+    
+    //ピクセル単位をタイル単位へ変更
+    public static int pixelsToTiles(double pixels) {
+        return (int)Math.floor(pixels/TILE_SIZE);
+    }
+    
+    //タイル単位をピクセル単位へ変更
+    public static int tilesToPixels(int tiles){
+        return tiles*TILE_SIZE;
+    }
+    
+}
